37ea33b97db5375f023ef9501f8a83ba29e541fa
Petr Volnov <neafiol.io@gmail.com>
2023-05-16 19:59:58 -0700

    readme

---
+++ README.md
@@ -0,0 +1,42 @@
+# Cache server IPFS images
+This server provides caching functionality for IPFS images using FastAPI and Docker.
+
+## Prerequisites
+Before deploying the service, ensure that you have the following prerequisites installed:
+
+- Docker
+- Docker Compose
+
+## Getting Started
+Setup configuration config.yml:
+
+Open the config.yml file and update the following configuration parameters:
+- `folder_size:` The maximum cache size in MB.
+- `cache_folder:` The path to the cache folder (default is ./cache).
+- `max_size:` The maximum number of images in the cache folder.
+Set cache folder path in `docker-compose.yml:`
+
+Open the docker-compose.yml file and update the volume mapping to your desired cache folder path:
+yaml
+Copy code
+```
+volumes:
+  - /var/www/here-storage/cache:/workdir/cache
+```
+Set up Nginx to publish images from the cache folder.
+
+## Run the server:
+
+`docker-compose up`
+
+##  Verify that the service is running:
+
+The service will be accessible at http://0.0.0.0:7001.
+
+You can make requests to the server by replacing the image link with `https://<link to your server>/<url>.` 
+
+For example: http://0.0.0.0:7001/nftstorage.link/ipfs/bafybeieboqph4qqf2n7lasq4ehn6snke2nhdqzde4i4hlywwd3dd7mcjma/U1307.png.
+
+## Contributing
+
+Contributions are welcome! If you find any issues or want to contribute new features, feel
--- src/routes.py
+++ src/routes.py
@@ -2,6 +2,7 @@ import datetime
 import os
 
 import aiohttp
+import yaml
 from fastapi import APIRouter
 from loguru import logger
 from starlette.requests import Request
@@ -9,27 +10,76 @@ from starlette.responses import RedirectResponse
 
 router = APIRouter()
 
+with open("config.yml", "r") as config_file:
+    CONFIG = yaml.safe_load(config_file)
 
-def clean_storage(storage, maxsize=10000):
-    items = list(storage.items())
-    for name, _ in sorted(items, key=lambda x: x[1]):
-        if len(storage) < maxsize:
+
+def get_folder_size(folder_path):
+    """
+    Calculates the total size of a folder in megabytes.
+
+    Args:
+        folder_path (str): The path to the folder.
+
+    Returns:
+        float: The size of the folder in megabytes.
+    """
+    total_size = 0
+    for path, dirs, files in os.walk(folder_path):
+        for file in files:
+            file_path = os.path.join(path, file)
+            total_size += os.path.getsize(file_path)
+    return total_size / 1024 / 1024
+
+
+def clean_storage(storage, max_size=10000):
+    """
+    Cleans up the storage by removing items if the maximum size is exceeded.
+
+    Args:
+        storage (dict): The storage dictionary.
+        max_size (int): The maximum size of the storage.
+
+    Returns:
+        None
+    """
+    if len(storage) < max_size:
+        return
+
+    items = sorted(storage.items(), key=lambda x: x[1])
+    for name, _ in items:
+        if len(storage) < max_size:
+            storage["folder_size"] = get_folder_size(CONFIG["cache_folder"])
+            logger.info(f'folder_size: {storage["folder_size"]}')
             return
 
-        cache_path = os.path.join("./cache", name)
+        cache_path = os.path.join(CONFIG["cache_folder"], name)
         if os.path.exists(cache_path):
             logger.info(f"Removing {name}")
-            os.remove(cache_path, )
+            os.remove(cache_path)
         storage.pop(name, None)
 
 
 @router.get("/{path:path}")
-async def redirect_to_cache(
-    r: Request,
-    path: str,
-):
+async def redirect_to_cache(r: Request, path: str):
+    """
+    Endpoint to redirect requests to cached images.
+
+    Args:
+        r (Request): The incoming request.
+        path (str): The path to the image.
+
+    Returns:
+        RedirectResponse: The redirect response.
+    """
+    folder_size = r.app.extra["storage"].get("folder_size", 0)
+    if folder_size > CONFIG["folder_size"]:
+        logger.error("Not enough memory to cache images")
+        return RedirectResponse(url=f"https://{path}")
+
     name = path.replace("/", "-")
-    cache_path = os.path.join("./cache", name)
+    cache_path = os.path.join(CONFIG["cache_folder"], name)
+
     if not os.path.exists(cache_path):
         async with aiohttp.ClientSession() as session:
             try:
@@ -37,9 +87,7 @@ async def redirect_to_cache(
                     if response.status == 200:
                         with open(cache_path, "wb") as f:
                             f.write(await response.read())
-                        logger.info(
-                            f"Image {path} downloaded and saved to the cache folder."
-                        )
+                        logger.info(f"Image {path} downloaded and saved to the cache folder.")
                     else:
                         logger.info(f"Failed to download image from {path}")
                         return RedirectResponse(url=f"https://{path}")
@@ -47,5 +95,5 @@ async def redirect_to_cache(
                 logger.error(f"{e} {path}")
 
     r.app.extra["storage"][name] = datetime.datetime.utcnow().timestamp()
-    clean_storage(r.app.extra["storage"], maxsize=10000)
+    clean_storage(r.app.extra["storage"], max_size=CONFIG["max_size"])
     return RedirectResponse(url=f"https://storage.herewallet.app/cache/{name}")
